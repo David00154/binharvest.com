6268aca6d7bfd3f18717918de43cbdcb9d88f129
david00154 <davidbriggs00154@gmail.com>
2023-03-03 03:31:32 +0100

    made link fix for header sign up button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,7 +62,7 @@ const Header: FC<{ pathname: string }> = ({ pathname }) => {
                                 d="m229.7 218.3l-43.3-43.2a92.2 92.2 0 1 0-11.3 11.3l43.2 43.3a8.2 8.2 0 0 0 11.4 0a8.1 8.1 0 0 0 0-11.4ZM40 116a76 76 0 1 1 76 76a76.1 76.1 0 0 1-76-76Z" />
                         </svg>
                     </a>
-                    <a href="" id=""
+                    <a href="https://dashboard.binharvest.com/sign-up.php" id=""
                         className="text-[15px] text-white bg-custom-orange px-[25px] py-[17px] md:inline-block hidden hover:bg-white hover:text-custom-dark-primary custom-transition ml-[35px]">Create
                         Account</a>
                 </ul>
